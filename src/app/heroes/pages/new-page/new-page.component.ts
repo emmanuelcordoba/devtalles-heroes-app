1cb64b0ea2faa9b9053e2bbab59b87321b1f4383
Emmanuel Cordoba <emmanuelc27@gmail.com>
2023-07-29 18:41:15 -0300

    Getters dentro del componente

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from "@angular/forms";
-import { Publisher } from "../../interfaces/hero.interface";
+import {Hero, Publisher} from "../../interfaces/hero.interface";
+import {HeroesServices} from "../../services/heroes.services";
 
 @Component({
   selector: 'app-new-page',
@@ -25,11 +26,19 @@ export class NewPageComponent {
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
 
+  constructor(
+    private heroesService: HeroesServices
+  ) {}
+
+  get currentHero(): Hero
+  {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
   onSubmit(): void {
-    console.log({
-      formIsValid: this.heroForm.valid,
-      value: this.heroForm.value
-    })
+    if(this.heroForm.invalid) return;
+    //this.heroesService.updateHero(this.heroForm.value)
   }
 
 }
