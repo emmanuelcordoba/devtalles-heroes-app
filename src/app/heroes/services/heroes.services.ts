56e0ea25f6c589b3cd113e4a0f7912682ebb1e55
Emmanuel Cordoba <emmanuelc27@gmail.com>
2023-07-29 09:08:49 -0300

    HerosService - CRUD Endpoints

--- src/app/heroes/services/heroes.services.ts
+++ src/app/heroes/services/heroes.services.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {catchError, Observable, of} from "rxjs";
+import { catchError, Observable, of, map } from "rxjs";
 import { Hero } from "../interfaces/hero.interface";
 import { enviroments } from "../../../enviroments/enviroments";
 
@@ -28,6 +28,26 @@ export class HeroesServices {
   getSuggestions(query: string): Observable<Hero[]>
   {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=5`);
+  }
+
+  addHero(hero: Hero): Observable<Hero>
+  {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero>
+  {
+    if(!hero.id) throw Error('Hero id is required');
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  }
+
+  deleteHeroById(id: string): Observable<boolean>
+  {
+    return this.http.delete<boolean>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError(err => of(false)),
+        map(res => true)
+      );
   }
 
 
