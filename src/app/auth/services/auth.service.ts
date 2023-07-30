b5c6d7c82a59dee0a12e18fa88c190c7cebd2bbe
Emmanuel Cordoba <emmanuelc27@gmail.com>
2023-07-30 00:55:22 -0300

    AuthService - Servicio de Autenticación

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable, tap } from "rxjs";
+
+import { enviroments } from "../../../enviroments/enviroments";
+import { User } from "../intefaces/user.interface";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl = enviroments.baseUrl;
+  private user?: User;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  get currentUser() : User | undefined
+  {
+    if(!this.user) return undefined;
+    return structuredClone(this.user);
+  }
+
+  login(email: string, password: string): Observable<User>
+  {
+    // this.http.post<User>(`${this.baseUrl}/login`, {email, password})
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap(user => this.user = user),
+        tap(user =>  localStorage.setItem('token', user.id.toString()))
+      );
+  }
+
+
+}
